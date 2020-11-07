05cbc8d2d694ebd01f82889b015ec25ccbc20bd2
Damir Beylkhanov <dbeylkhanov@appulate.com>
2020-11-07 23:31:04 +0300

    add StronglyTypedIdentity.ArchTests.csproj
    add use-case in CustomType.cs which is reproducing an incorrect behavior by the NetArchTest

--- StronglyTypedIdentity/Domain/CustomType.cs
+++ StronglyTypedIdentity/Domain/CustomType.cs
@@ -1,7 +1,18 @@
+using StronglyTypedIdentity.Infrastructure;
+
 namespace StronglyTypedIdentity.Domain
 {
 	public class CustomType
 	{
 		public CustomTypeId Id { get; }
+
+		public CustomType()
+		{
+			// todo: uncomment the 12th line and test will be failed!
+			//var dbContext = new CustomDbContext(); // Expected: Failed vs. Actual: Failed
+			
+			// todo: uncomment the 15th line and test will be passed! Looks like NetArchTest missed the `nameof` exp., even though the CustomType depends on the StronglyTypedIdentity.Infrastructure namespace anyway
+			var dbContextName = nameof(CustomDbContext); // Expected: Failed vs. Actual: Success 
+		}
 	}
 }
